78c24e60100d419cbd86d4a8f7d7dee5669f64d2
Andrea Gulino <andrea.gulino@polimi.it>
2019-08-16 11:57:25 +0200

    Change sombrero > mutviz

--- static/js/api.js
+++ static/js/api.js
@@ -1,5 +1,5 @@
-var API_ROOT = "/sombrero/";
-//var API_ROOT = "http://127.0.0.1:5000/sombrero/";
+var API_ROOT = "/mutviz/";
+//var API_ROOT = "http://geco.deib.polimi.it/mutviz/";
 
 var API_L01 = API_ROOT+"api/tumor_types/";
 var API_L02 = API_ROOT+"api/repository/";
