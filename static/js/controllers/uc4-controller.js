cfe043e71c5bef81e9a3420041ffa8f507b56fcd
Andrea Gulino <andrea.gulino@polimi.it>
2020-04-27 22:07:09 +0200

    fix data loss on tumor change

--- static/js/controllers/uc4-controller.js
+++ static/js/controllers/uc4-controller.js
@@ -78,7 +78,7 @@ app.controller('uc4_ctrl', function($scope, $rootScope, $routeParams, $timeout, 
 
 
         if( filename in $scope.uc4_files && "result" in $scope.uc4_files[filename] ) 
-        $scope.load( $scope.uc4_files[filename])
+            $scope.load( $scope.uc4_files[filename].result)
         else {
 
             request_body = {
