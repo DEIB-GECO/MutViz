713c3d488189d229e4d682d8024d8ffc485e8d21
Andrea Gulino <andrea.gulino@polimi.it>
2020-05-20 01:37:17 +0200

    Update signatures_refitting.py

--- api/signatures/signatures_refitting.py
+++ api/signatures/signatures_refitting.py
@@ -164,6 +164,7 @@ def get_refitting( mut_df, user_file_df,  sigs_df_norm = None):
     categories = list(mut_df.columns)
     M = mut_df.values
 
+
     if  sigs_df_norm == None:
 
         filename = os.path.join(dirname, "signatures.csv")
