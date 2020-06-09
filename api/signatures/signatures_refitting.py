8712c22a5e9fd43fe29208c2bde45eed5ea7e9f6
Andrea Gulino <andrea.gulino@polimi.it>
2020-06-09 03:40:16 +0200

    Update signatures_refitting.py

--- api/signatures/signatures_refitting.py
+++ api/signatures/signatures_refitting.py
@@ -93,7 +93,7 @@ def frequencies(df):
         for i in range(len_reg - 2):
             triplet = row['sequence'][i:i + 3]
             local_tot += 1
-            if triplet in codon_freq:
+            if triplet in local_codon_freq:
                 local_codon_freq[triplet] += 1
             else:
                 local_codon_freq[triplet] = 1
