34d5b1b7486f75759e55c553e0f5c59c68f038be
Andrea Gulino <andrea.gulino@polimi.it>
2020-06-25 11:50:55 +0200

    update version

--- static/js/app.js
+++ static/js/app.js
@@ -5,41 +5,41 @@ app.config(['$routeProvider', '$locationProvider',
 
     $routeProvider
       .when('/home', {
-        templateUrl: 'views/home.html?v=1',
+        templateUrl: 'views/home.html?v=1.1',
         controller: 'home_ctrl',
         controllerAs: 'home'
       })
       .when('/data', {
-        templateUrl: 'views/data.html?v=1',
+        templateUrl: 'views/data.html?v=1.1',
         controller: 'data_ctrl',
         controllerAs: 'data'
       })
       .when('/uc1', {
-        templateUrl: 'views/uc1.html?v=1',
+        templateUrl: 'views/uc1.html?v=1.1',
         controller: 'uc1_ctrl',
         controllerAs: 'uc1c'
       })
       .when('/uc2', {
-        templateUrl: 'views/uc2.html?v=1',
+        templateUrl: 'views/uc2.html?v=1.1',
         controller: 'uc2_ctrl',
         controllerAs: 'uc2c'
       })
       .when('/uc3', {
-        templateUrl: 'views/uc3.html?v=1',
+        templateUrl: 'views/uc3.html?v=1.1',
         controller: 'uc3_ctrl',
         controllerAs: 'uc3c'
       })
       .when('/uc4', {
-        templateUrl: 'views/uc4.html?v=1',
+        templateUrl: 'views/uc4.html?v=1.1',
         controller: 'uc4_ctrl',
         controllerAs: 'uc4c'
       })
       .when('/uc5', {
-        templateUrl: 'views/uc5.html?v=1',
+        templateUrl: 'views/uc5.html?v=1.1',
         controller: 'uc5_ctrl',
         controllerAs: 'uc5c'
       }).when('/uc6', {
-        templateUrl: 'views/uc6.html?v=1',
+        templateUrl: 'views/uc6.html?v=1.1',
         controller: 'uc6_ctrl',
         controllerAs: 'uc6c'
       }).otherwise('/home');
